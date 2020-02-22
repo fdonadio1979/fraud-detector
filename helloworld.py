4cb1a888d2aca989c453d425d05be25813ef2ee4
Ing. Diego Fabián Tondo <diego.tondo@cintelink.com.ar>
2020-02-22 20:53:38 -0300

    maps example

--- helloworld.py
+++ helloworld.py
@@ -4,9 +4,8 @@ import numpy as np
 
 st.title('Uber pickups in NYC')
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+DATE_COLUMN = 'TIME'
+DATA_URL = '29148163867.csv'
 
 @st.cache
 def load_data(nrows):

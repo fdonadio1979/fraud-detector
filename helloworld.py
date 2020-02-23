599ce4392d85d01c97dbcca9e7be1e90711545e8
Ing. Diego Fabián Tondo <diego.tondo@cintelink.com.ar>
2020-02-23 13:42:30 -0300

    playing with cache

--- helloworld.py
+++ helloworld.py
@@ -12,7 +12,8 @@ def load_data_distance(nrows):
     data_d = pd.read_csv("raw-data.csv", nrows=nrows, index_col = "DISTANCE")
     return (data_d)
 
-nrows = st.number_input('Insert a number', max_value=10000, min_value=0, value=100)
+# nrows = st.number_input('Insert a number', max_value=10000, min_value=0, value=100)
+nrows = st.slider('hour', max_value=11000, min_value=0, value=100)
 
 df1 = load_data_time(nrows)
 if st.checkbox('Time/Distance Index'):

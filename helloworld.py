350b93e47f769ff81ea7aaaaca4b8f87f59edaf1
Ing. Diego Fabián Tondo <diego.tondo@cintelink.com.ar>
2020-02-23 12:12:56 -0300

    maps example

--- helloworld.py
+++ helloworld.py
@@ -3,15 +3,18 @@ import pandas as pd
 import numpy as np
 
 @st.cache
-Km = st.checkbox('Km')
-df1 = pd.read_csv("raw-data.csv",index_col = "TIME")
-if Km:
-    df1 = pd.read_csv("raw-data.csv",index_col = "DISTANCE")
+def fetch_data():
+    Km = st.checkbox('Km')
+    df1 = pd.read_csv("raw-data.csv",index_col = "TIME")
+    if Km:
+        df1 = pd.read_csv("raw-data.csv",index_col = "DISTANCE")
+return (data)
     
+df2 = fetch_data()
 if st.checkbox('Show dataframe'):
-    st.write(df1)
+    st.write(df2)
 # st.map(df1)
-st.area_chart(df1['HEIGHT'])
+st.area_chart(df2['HEIGHT'])
 
 # df2 = pd.read_csv("raw-data.csv")
 # st.area_chart(df2)

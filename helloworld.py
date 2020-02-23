6d109fdd0dcfeeed1b074cdf528c6ab86006ea0f
Ing. Diego Fabián Tondo <diego.tondo@cintelink.com.ar>
2020-02-23 12:16:46 -0300

    playing with cache

--- helloworld.py
+++ helloworld.py
@@ -2,19 +2,19 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+Km = st.checkbox('Km')
 @st.cache
 def fetch_data():
-    Km = st.checkbox('Km')
-    df1 = pd.read_csv("raw-data.csv",index_col = "TIME")
+    data = pd.read_csv("raw-data.csv",index_col = "TIME")
     if Km:
-        df1 = pd.read_csv("raw-data.csv",index_col = "DISTANCE")
+        data = pd.read_csv("raw-data.csv",index_col = "DISTANCE")
     return (data)
     
-df2 = fetch_data()
+df1 = fetch_data()
 if st.checkbox('Show dataframe'):
-    st.write(df2)
+    st.write(df1)
 # st.map(df1)
-st.area_chart(df2['HEIGHT'])
+st.area_chart(df1['HEIGHT'])
 
 # df2 = pd.read_csv("raw-data.csv")
 # st.area_chart(df2)

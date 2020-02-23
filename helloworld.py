9e4988634a0dcef36b264e5e7fc1c2e750a4404a
Ing. Diego Fabián Tondo <diego.tondo@cintelink.com.ar>
2020-02-23 10:49:07 -0300

    maps example

--- helloworld.py
+++ helloworld.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv("raw-data.csv", columns=['INDEX', 'DATE', 'TIME', 'latitude', 'longitude', 'HEIGHT', 'SPEED', 'DISTANCE'])
+df = pd.read_csv("raw-data.csv")
 if st.checkbox('Show dataframe'):
     st.write(df)
 st.map(df)

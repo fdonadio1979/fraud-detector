6e527fd6c352f8c1d61bc816bbd373a4c83f817f
Ing. Diego Fabián Tondo <diego.tondo@cintelink.com.ar>
2020-02-23 09:58:41 -0300

    maps example

--- helloworld.py
+++ helloworld.py
@@ -7,4 +7,8 @@ if st.checkbox('Show dataframe'):
     st.write(df)
 st.map(df)
 
-st.area_chart(df)
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+st.area_chart(chart_data)

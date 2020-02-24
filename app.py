1d8b2141a8ce0f3b27a3e0e13d090c5366ac7b29
Ing. Diego Fabián Tondo <diego.tondo@cintelink.com.ar>
2020-02-24 12:59:24 -0300

    trying level1+ plot

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ import pydeck as pdk
 
 
 @st.cache
-def load_data_time(nrows):
-    data_t = pd.read_csv("raw-data1.csv", nrows=nrows, index_col = "Hours")
+def load_data_time(nrows, skiprows):
+    data_t = pd.read_csv("raw-data1.csv", nrows=nrows, skiprows=skiprows, index_col = "Hours")
     return (data_t)
 
 @st.cache
-def load_data_distance(nrows):
-    data_d = pd.read_csv("raw-data1.csv", nrows=nrows, index_col = "Distance")
+def load_data_distance(nrows, skiprows):
+    data_d = pd.read_csv("raw-data1.csv", nrows=nrows, skiprows=skiprows, index_col = "Distance")
     return (data_d)
 
 # nrows = st.number_input('Insert a number', max_value=10000, min_value=0, value=100)
@@ -20,21 +20,21 @@ def load_data_distance(nrows):
 
 
 if st.checkbox('Time/Distance Index'):
-    df1 = load_data_distance(1650)
+    df1 = load_data_distance(1650,0)
     max_value = df1.index[-1].round(decimals=1)
     sel_value = st.slider('Distance [Km]', 0.1, max_value, (0.1, max_value))
-    nrows = int (sel_value[0] * 1650 / max_value)
-    skiprows = int (sel_value[1] * 1650 / max_value)
-    df1 = load_data_distance(nrows)
+    nrows = int (sel_value[1] * 1650 / max_value)
+    skiprows = int (sel_value[0] * 1650 / max_value)
+    df1 = load_data_distance(nrows,skiprows)
 else:
-    df1 = load_data_time(1650)
+    df1 = load_data_time(1650,0)
     max_value = df1.index[-1].round(decimals=1)
     sel_value = st.slider('Time [Hours]', 0.1, max_value, (0.1, max_value))
-    nrows = int (sel_value[0] * 1650 / max_value)
+    nrows = int (sel_value[1] * 1650 / max_value)
     st.write(nrows)
-    skiprows = int (sel_value[1] * 1650 / max_value)
+    skiprows = int (sel_value[0] * 1650 / max_value)
     st.write(skiprows)
-    df1 = load_data_time(nrows)
+    df1 = load_data_time(nrows,skiprows)
 
 
 

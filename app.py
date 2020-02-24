052d8b9e03a1f820c2fc3cbf98aecb9a8b89b2a5
Ing. Diego Fabián Tondo <diego.tondo@cintelink.com.ar>
2020-02-23 21:58:44 -0300

    trying new raw data

--- app.py
+++ app.py
@@ -6,30 +6,25 @@ import pydeck as pdk
 
 DATE_COLUMN = 'date/time'
 
-@st.cache
-def load_data_time(nrows):
-    data_t = pd.read_csv("raw-data1.csv", nrows=nrows, index_col = "Date/Time")
+@st.cache(persist=True)
+def load_data(nrows):
+    data = pd.read_csv("raw-data1.csv", nrows=nrows)
     lowercase = lambda x: str(x).lower()
-    data_t.rename(lowercase, axis='columns', inplace=True)
-    data_t[DATE_COLUMN] = pd.to_datetime(data_t[DATE_COLUMN])
-    return (data_t)
+    data.rename(lowercase, axis="columns", inplace=True)
+    data[DATE_TIME] = pd.to_datetime(data[DATE_TIME])
+    return data
 
-@st.cache
-def load_data_distance(nrows):
-    data_d = pd.read_csv("raw-data1.csv", nrows=nrows, index_col = "Distance")
-    lowercase = lambda x: str(x).lower()
-    data_d.rename(lowercase, axis='columns', inplace=True)
-    data_d[DATE_COLUMN] = pd.to_datetime(data_d[DATE_COLUMN])
-    return (data_d)
 
-# nrows = st.number_input('Insert a number', max_value=1652, min_value=0, value=100)
 nrows = st.slider('track', max_value=1652, min_value=0, value=1652)
 
-df1 = load_data_distance(nrows)
-if st.checkbox('Time/Distance Index'):
-    df1 = load_data_time(nrows)
+data = load_data(nrows)
 
-midpoint = (np.average(df1["lat"]), np.average(df1["lon"]))
+hour = st.slider("Hour to look at", 0, 23)
+
+data = data[data[DATE_TIME].dt.hour == hour]
+
+st.subheader("Geo data between %i:00 and %i:00" % (hour, (hour + 1) % 24))
+midpoint = (np.average(data["lat"]), np.average(data["lon"]))
 
 st.write(pdk.Deck(
     map_style="mapbox://styles/mapbox/light-v9",
@@ -42,7 +37,7 @@ st.write(pdk.Deck(
     layers=[
         pdk.Layer(
             "HexagonLayer",
-            data=df1,
+            data=data,
             get_position=["lon", "lat"],
             radius=100,
             elevation_scale=4,
@@ -52,11 +47,63 @@ st.write(pdk.Deck(
         ),
     ],
 ))
-# st.altair_chart(alt.Chart(df1))
 
-# st.map(df1,11)
-st.area_chart(df1['speed'])
-st.area_chart(df1['voltotal'])
+st.area_chart(data['speed'])
+st.area_chart(data['voltotal'])
+
+
+
+# @st.cache
+# def load_data_time(nrows):
+    # data_t = pd.read_csv("raw-data1.csv", nrows=nrows, index_col = "Date/Time")
+    # lowercase = lambda x: str(x).lower()
+    # data_t.rename(lowercase, axis='columns', inplace=True)
+    # data_t[DATE_COLUMN] = pd.to_datetime(data_t[DATE_COLUMN])
+    # return (data_t)
+
+# @st.cache
+# def load_data_distance(nrows):
+    # data_d = pd.read_csv("raw-data1.csv", nrows=nrows, index_col = "Distance")
+    # lowercase = lambda x: str(x).lower()
+    # data_d.rename(lowercase, axis='columns', inplace=True)
+    # data_d[DATE_COLUMN] = pd.to_datetime(data_d[DATE_COLUMN])
+    # return (data_d)
+
+# # nrows = st.number_input('Insert a number', max_value=1652, min_value=0, value=100)
+# nrows = st.slider('track', max_value=1652, min_value=0, value=1652)
+
+# df1 = load_data_distance(nrows)
+# if st.checkbox('Time/Distance Index'):
+    # df1 = load_data_time(nrows)
+
+# midpoint = (np.average(df1["lat"]), np.average(df1["lon"]))
+
+# st.write(pdk.Deck(
+    # map_style="mapbox://styles/mapbox/light-v9",
+    # initial_view_state={
+        # "latitude": midpoint[0],
+        # "longitude": midpoint[1],
+        # "zoom": 11,
+        # "pitch": 50,
+    # },
+    # layers=[
+        # pdk.Layer(
+            # "HexagonLayer",
+            # data=df1,
+            # get_position=["lon", "lat"],
+            # radius=100,
+            # elevation_scale=4,
+            # elevation_range=[0, 1000],
+            # pickable=True,
+            # extruded=True,
+        # ),
+    # ],
+# ))
+# # st.altair_chart(alt.Chart(df1))
+
+# # st.map(df1,11)
+# st.area_chart(df1['speed'])
+# st.area_chart(df1['voltotal'])
 
 
 

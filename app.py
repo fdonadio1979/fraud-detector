57eb8bdc870971256ee0060000a3976a0f3320e6
Ing. Diego Fabián Tondo <diego.tondo@cintelink.com.ar>
2020-02-24 13:01:23 -0300

    trying level1+ plot

--- app.py
+++ app.py
@@ -20,14 +20,14 @@ def load_data_distance(nrows, skiprows):
 
 
 if st.checkbox('Time/Distance Index'):
-    df1 = load_data_distance(1650,0)
+    df1 = load_data_distance(1650,1)
     max_value = df1.index[-1].round(decimals=1)
     sel_value = st.slider('Distance [Km]', 0.1, max_value, (0.1, max_value))
     nrows = int (sel_value[1] * 1650 / max_value)
     skiprows = int (sel_value[0] * 1650 / max_value)
     df1 = load_data_distance(nrows,skiprows)
 else:
-    df1 = load_data_time(1650,0)
+    df1 = load_data_time(1650,1)
     max_value = df1.index[-1].round(decimals=1)
     sel_value = st.slider('Time [Hours]', 0.1, max_value, (0.1, max_value))
     nrows = int (sel_value[1] * 1650 / max_value)

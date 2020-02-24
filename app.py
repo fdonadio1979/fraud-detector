f1b29611c8d4d14a0875283bc5decb9980547e08
Ing. Diego Fabián Tondo <diego.tondo@cintelink.com.ar>
2020-02-24 11:00:58 -0300

    trying new raw data

--- app.py
+++ app.py
@@ -4,27 +4,25 @@ import numpy as np
 import altair as alt
 import pydeck as pdk
 
-DATE_TIME = "date/time"
 
-@st.cache(persist=True)
-def load_data(nrows):
-    data = pd.read_csv("raw-data1.csv", nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis="columns", inplace=True)
-    data[DATE_TIME] = pd.to_datetime(data[DATE_TIME])
-    return data
+@st.cache
+def load_data_time(nrows):
+    data_t = pd.read_csv("raw-data1.csv", nrows=nrows, index_col = "Hours")
+    return (data_t)
 
+@st.cache
+def load_data_distance(nrows):
+    data_d = pd.read_csv("raw-data1.csv", nrows=nrows, index_col = "Distance")
+    return (data_d)
 
+# nrows = st.number_input('Insert a number', max_value=10000, min_value=0, value=100)
 nrows = st.slider('track', max_value=1652, min_value=0, value=1652)
 
-data = load_data(nrows)
+df1 = load_data_time(nrows)
+if st.checkbox('Time/Distance Index'):
+    df1 = load_data_distance(nrows)
 
-hour = st.slider("Hour to look at", 0, 23)
-
-data = data[data[DATE_TIME].dt.hour == hour]
-
-st.subheader("Geo data between %i:00 and %i:00" % (hour, (hour + 1) % 24))
-midpoint = (np.average(data["lat"]), np.average(data["lon"]))
+midpoint = (np.average(df1["Lat"]), np.average(df1["Lon"]))
 
 st.write(pdk.Deck(
     map_style="mapbox://styles/mapbox/light-v9",
@@ -37,8 +35,8 @@ st.write(pdk.Deck(
     layers=[
         pdk.Layer(
             "HexagonLayer",
-            data=data,
-            get_position=["lon", "lat"],
+            data=df1,
+            get_position=["Lon", "Lat"],
             radius=100,
             elevation_scale=4,
             elevation_range=[0, 1000],
@@ -47,84 +45,8 @@ st.write(pdk.Deck(
         ),
     ],
 ))
+# st.altair_chart(alt.Chart(df1))
 
-st.area_chart(data['speed'])
-st.area_chart(data['voltotal'])
-
-
-
-# @st.cache
-# def load_data_time(nrows):
-    # data_t = pd.read_csv("raw-data1.csv", nrows=nrows, index_col = "Date/Time")
-    # lowercase = lambda x: str(x).lower()
-    # data_t.rename(lowercase, axis='columns', inplace=True)
-    # data_t[DATE_COLUMN] = pd.to_datetime(data_t[DATE_COLUMN])
-    # return (data_t)
-
-# @st.cache
-# def load_data_distance(nrows):
-    # data_d = pd.read_csv("raw-data1.csv", nrows=nrows, index_col = "Distance")
-    # lowercase = lambda x: str(x).lower()
-    # data_d.rename(lowercase, axis='columns', inplace=True)
-    # data_d[DATE_COLUMN] = pd.to_datetime(data_d[DATE_COLUMN])
-    # return (data_d)
-
-# # nrows = st.number_input('Insert a number', max_value=1652, min_value=0, value=100)
-# nrows = st.slider('track', max_value=1652, min_value=0, value=1652)
-
-# df1 = load_data_distance(nrows)
-# if st.checkbox('Time/Distance Index'):
-    # df1 = load_data_time(nrows)
-
-# midpoint = (np.average(df1["lat"]), np.average(df1["lon"]))
-
-# st.write(pdk.Deck(
-    # map_style="mapbox://styles/mapbox/light-v9",
-    # initial_view_state={
-        # "latitude": midpoint[0],
-        # "longitude": midpoint[1],
-        # "zoom": 11,
-        # "pitch": 50,
-    # },
-    # layers=[
-        # pdk.Layer(
-            # "HexagonLayer",
-            # data=df1,
-            # get_position=["lon", "lat"],
-            # radius=100,
-            # elevation_scale=4,
-            # elevation_range=[0, 1000],
-            # pickable=True,
-            # extruded=True,
-        # ),
-    # ],
-# ))
-# # st.altair_chart(alt.Chart(df1))
-
-# # st.map(df1,11)
-# st.area_chart(df1['speed'])
-# st.area_chart(df1['voltotal'])
-
-
-
-# @st.cache
-# def fetch_data(SW):
-    # data = pd.read_csv("raw-data.csv",index_col = "TIME")
-    # if Km:
-        # data = pd.read_csv("raw-data.csv",index_col = "DISTANCE")
-    # return (data)
-    
-# df1 = fetch_data()
-# if st.checkbox('Show dataframe'):
-    # st.write(df1)
-# # st.map(df1)
-# st.area_chart(df1['HEIGHT'])
-
-# df2 = pd.read_csv("raw-data.csv")
-# st.area_chart(df2)
-
-# chart_data = pd.DataFrame(
-    # np.random.randn(20, 3),
-    # columns=['a', 'b', 'c'])
-
-# st.area_chart(chart_data)
+# st.map(df1,11)
+st.area_chart(df1['Speed'])
+st.area_chart(df1['VolTotal'])

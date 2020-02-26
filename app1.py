074d442be35ec8c2a62429d909d60f9d81350f28
fdonadio <fdonadio@gmail.com>
2020-02-26 10:47:20 -0300

    trying optimize

--- app1.py
+++ app1.py
@@ -41,23 +41,12 @@ tank = st.sidebar.radio(
     ('All', 'Tank1', 'Tank2', 'Tank3', 'Tank4', 'Tank5', 'Tank6'))
 
 if tank == 'All':
-    df1 =  pd.DataFrame(
-	[[supplies.values[1,12],supplies.values[8,8],3,74,22,2.95],
-	['Feb',61,45,78,26,3.02],
-	['Mar',65,48,84,25,2.34],
-	['Apr',67,50,92,28,1.02],
-	['May',71,53,98,35,0.48],
-	['Jun',75,56,107,41,0.11],
-	['Jul',77,58,105,44,0.0],
-	['Aug',77,59,102,43,0.03],
-	['Sep',77,57,103,40,0.17],
-	['Oct',73,54,96,34,0.81],
-	['Nov',64,48,84,30,1.7],
-	['Dec',58,42,73,21,2.56]],
-	index = [0,1,2,3,4,5,6,7,8,9,10,11],
-	columns = ['month','avg_high','avg_low','record_high','record_low','avg_precipitation'])
-
-
+    df1 =  df.loc[0,['Loading Date','Unloading Date','Elapsed Time']]
+else if tank == 'Tank1':
+    df1 =  df.loc[1,['Loading Date','Unloading Date','Elapsed Time']]
+else:
+    df1 =  df.loc[2,['Loading Date','Unloading Date','Elapsed Time']]
+	
 st.write(df)
 st.write(max_index)
 st.write(max_distance)
@@ -67,9 +56,7 @@ st.write(from_row)
 st.write(to_row)
 
 st.write(label)
-st.write(supplies.values[0,15])
-st.write(supplies.values[6,15])
-st.write(supplies.values[0,15]+supplies.values[6,15])
+st.write(df1)
 
 # @st.cache
 # def load_data_time(nrows, skiprows):

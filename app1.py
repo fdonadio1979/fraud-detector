ec783c3e50752d34ef11703ff92d2358e01570f3
fdonadio <fdonadio@gmail.com>
2020-02-26 11:28:20 -0300

    trying optimize

--- app1.py
+++ app1.py
@@ -43,7 +43,7 @@ tank = st.sidebar.radio(
     ('All', 'Tank1', 'Tank2', 'Tank3', 'Tank4', 'Tank5', 'Tank6'))
 
 if tank == 'All':
-    df1 =  supplies.loc[0,['Loading Date','Unloading Date','Elapsed Time']]
+    df1 =  supplies[0,['Loading Date','Unloading Date','Elapsed Time']]
 elif tank == 'Tank1':
     df1 =  supplies.loc[1,['Loading Date','Unloading Date','Elapsed Time']]
 else:

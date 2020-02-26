acb5da2e19fcee39b6b31115e9b35a688c993fcd
Ing. Diego Fabián Tondo <diego.tondo@cintelink.com.ar>
2020-02-25 23:21:09 -0300

    trying optimize

--- app1.py
+++ app1.py
@@ -16,8 +16,8 @@ df = load_data(csv_file)
 
 
 max_index = df.index[-1]
-max_distance = df['Distance'].iloc[-1]
-max_time = df['Hours'].iloc[-1]
+max_distance = df['Distance'].iloc[-1].round(decimals=1)
+max_time = df['Hours'].iloc[-1].round(decimals=1)
 
 
 st.write(df)

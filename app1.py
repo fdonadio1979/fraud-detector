035ce124e228362688e1895d3f2bfaa2c7df987a
Ing. Diego Fabián Tondo <diego.tondo@cintelink.com.ar>
2020-02-26 00:44:52 -0300

    trying optimize

--- app1.py
+++ app1.py
@@ -42,7 +42,7 @@ tank = st.sidebar.radio(
 
 if tank == 'All':
     df1 =  pd.DataFrame(
-	[['Jan',58,42,74,22,2.95],
+	[[supplies.values[1,12],58,42,74,22,2.95],
 	['Feb',61,45,78,26,3.02],
 	['Mar',65,48,84,25,2.34],
 	['Apr',67,50,92,28,1.02],
@@ -67,7 +67,7 @@ st.write(from_row)
 st.write(to_row)
 
 st.write(label)
-st.write(supplies.values[0,6])
+st.write(supplies.values[1,12])
 
 
 # @st.cache
